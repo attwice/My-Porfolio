d63b2cbcf4f3f6ec932b879d21db7c52216e4f3a
Billibear <93044176+attwice@users.noreply.github.com>
2022-03-17 01:34:40 +0900

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://www.freelancer.com/freelancers/skills/illustration-react-js-blockchain-non-fungible-tokens?w=f&ngsw-bypass="
+                href="https://www.freelancer.com/freelancers/skills/react-js-blockchain-react-js-framework-non-fungible-tokens?w=f&ngsw-bypass="
                 target="_blank"
                 rel="noreferrer"
               >
